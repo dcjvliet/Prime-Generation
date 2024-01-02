f323b27f4cd33dae50e6a3c15c7e67aafdf503c7
Minestone4306 <78621484+dcjvliet@users.noreply.github.com>
2024-01-01 21:02:23 -0500

    Update v_3.py
    Edited to remove the Google Sheets aspect of the code.

--- DownloadThis/v_3.py
+++ DownloadThis/v_3.py
@@ -1,28 +1,15 @@
 import time
 import math
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 
-
-scopes = [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive'
-]
-
-creds = ServiceAccountCredentials.from_json_keyfile_name('C:\\Users\\jacks\\OneDrive\\Desktop\\Coding\\Prime Generator\\secret_key.json', scopes=scopes)
-
-file = gspread.authorize(creds)
-workbook = file.open('Prime Generation')
-sheet = workbook.worksheet('v_3')
 
 runs = 0
 while runs < 100:
-    num = 10
+    num_primes = 10
     prime_list = [2, 3, 5]
     primes = 3
     start_time = time.perf_counter_ns()
-    while primes < num:
-        for i in range(7, (2 ** num), 2):
+    while primes < num_primes:
+        for i in range(7, (2 ** num_primes), 2):
             sum = 0
             limit = math.ceil(math.sqrt(i + 1))
             last_digit = int(repr(i)[-1])
@@ -37,10 +24,9 @@ while runs < 100:
             if sum == 0:
                 prime_list.append(i)
                 primes += 1
-            if len(prime_list) == num:
+            if len(prime_list) == num_primes:
                 break
     end_time = time.perf_counter_ns()
     overall_time = end_time - start_time
-    cell = f'B{(runs + 3)}'
-    sheet.update_acell(cell, overall_time)
+    print(overall_time)
     runs += 1
